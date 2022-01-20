47657f49f8491af2506aa3a892e4065076700295
motion <motion@motions-MacBook-Pro.local>
2022-01-20 10:18:34 +0700

    fix sales

--- frontend/src/pages/Sales.vue
+++ frontend/src/pages/Sales.vue
@@ -2,7 +2,7 @@
     <div class="row">
         <button class="btn" @click="add">Add</button>
         <vue-suggestion :items="customers" v-model="form.customer_id" , :setLabel="setLabel" , :itemTemplate="itemTemplate" , @onInputChange="inputChange" , @onItemSelected="itemSelected">
-                            </vue-suggestion>
+        </vue-suggestion>
         <modal name="addmodal" transition="pop-out" :width="modalWidth" :focus-trap="true" :height="500">
             <div class="box">
                 <div class="partition" id="partition-register">
@@ -10,17 +10,23 @@
                     <div class="partition-form">
                         <form autocomplete="false">
                             <input id="n-trx" type="text" placeholder="Code Transaksi" v-model="form.code_transaksi">
-                            <input id="n-trx" type="text" placeholder="Customer ID" v-model="form.customer_id">
-                            
+                            <select id="n-tipe" name="unit" v-model="form.customer_id" placeholder="Unit">
+                            <option selected value="">Pilih User</option>
+                                          <option v-for="item in listCustomers" :value="item.id" :key="item.id">
+                  {{ item.nama }}
+                </option>
+                                                                </select>
+    
                             <div class="form-group row" v-for="(item, k) in form.items" :key="k">
                                 <div class="col-md-5">
-                                    <input id="item" type="text" class="form-control" name="item" v-model="form.items[k].item_id" placeholder="Item">
+                                <select id="n-tipe" name="unit" v-model="form.items[k].item_id" placeholder="Unit">
+                                <option selected value="">Pilih Item</option>
+                                          <option v-for="item in listItems" :value="item.id" :key="item.id">
+                  {{ item.nama_item }}
+                </option></select>
                                 </div>
                                 <div class="col-md-5">
-                                    <input id="item" type="text" class="form-control" name="qty" v-model="form.items[k].qty" placeholder="Qty">
-                                </div>
-                                <div class="col-md-2">
-                                    <button v-if="k > 0" @click.prevent="remove(k)"><i class="fa fa-trash"></i></button>
+                                    <input id="item" type="number" class="form-control" name="qty" v-model="form.items[k].qty" placeholder="Qty">
                                 </div>
                             </div>
                             <div class="col-md-12 text-right">
@@ -46,32 +52,32 @@
                         </thead>
                         <tbody>
                             <template v-for="(item, index) in tableData">
-                                                  <tr>
-                                                    <td>{{item.id}}</td>
-                                                    <td>{{item.code_transaksi}}</td>
-                                                    <td>{{item.tanggal_transaksi}}</td>
-                                                    <td>{{item.customer.nama}}</td>
-                                                    <td>{{item.total_diskon}}</td>
-                                                    <td><b>{{item.total_bayar}}</b></td>
-                                                    
-                                                    <td>{{new Date(item.updated_at).toLocaleString('en-GB')}}</td>
-                                                    <td>
-                                                      <p-button type="danger" outline icon><i class="fa fa-trash" @click="remove(item.id)"></i></p-button>
-                                                    </td>
-                                                  </tr>
-                                                  <tr style="border:0">
-                                                    <td colspan=7>
-                                                      <table width=100% border=0>
-                                                        <tr v-for="(item, index) in item.items">
-                                                          <td>{{item.nama_item}}</td>
-                                                          <td><img v-bind:src="apiurl+item.barang" style="width:70px"></td>
-                                                          <td>{{item.qty}}({{item.unit}})</td>
-                                                          <td>{{item.harga_satuan}}</td>
-                                                          <td>{{item.total_harga}}</td>
-                                                        </tr>
-                                                      </table>
-                                                    </td>
-                                                  </tr>
+                                                                                  <tr>
+                                                                                    <td>{{item.id}}</td>
+                                                                                    <td>{{item.code_transaksi}}</td>
+                                                                                    <td>{{item.tanggal_transaksi}}</td>
+                                                                                    <td>{{item.customer.nama}}</td>
+                                                                                    <td>{{item.total_diskon}}</td>
+                                                                                    <td><b>{{item.total_bayar}}</b></td>
+                                                                                    
+                                                                                    <td>{{new Date(item.updated_at).toLocaleString('en-GB')}}</td>
+                                                                                    <td>
+                                                                                      <p-button type="danger" outline icon><i class="fa fa-trash" @click="remove(item.id)"></i></p-button>
+                                                                                    </td>
+                                                                                  </tr>
+                                                                                  <tr style="border:0">
+                                                                                    <td colspan=7>
+                                                                                      <table width=100% border=0 style="background:#f1f1f1;">
+                                                                                        <tr v-for="(item, index) in item.items">
+                                                                                          <td>{{item.nama_item}}</td>
+                                                                                          <td><img v-bind:src="apiurl+item.barang" style="width:70px"></td>
+                                                                                          <td>{{item.qty}}({{item.unit}})</td>
+                                                                                          <td>{{item.harga_satuan}}</td>
+                                                                                          <td>{{item.total_harga}}</td>
+                                                                                        </tr>
+                                                                                      </table>
+                                                                                    </td>
+                                                                                  </tr>
 </template>
               
               
@@ -85,7 +91,7 @@
 
 <script>
 import axios from 'axios';
-import VueSuggestion from 'vue-suggestion' 
+import VueSuggestion from 'vue-suggestion'
 import itemTemplate from './ItemTemplate.vue';
 
 const tableColumns = ["Id", "Code TRX", "tanggal", "customer name", "Diskon", "Total", "updated", "action"];
@@ -96,11 +102,13 @@ let tableData = [];
 let stateform = 'add';
 
 export default {
-components: {
-    VueSuggestion
-  },
+    components: {
+        VueSuggestion
+    },
     mounted() {
         this.getData();
+        this.getCustomers();
+        this.getItems();
     },
     methods: {
         getData() {
@@ -108,6 +116,22 @@ components: {
                 .then((result) => {
                     this.tableData = result.data.data;
                     console.log(result);
+                }).catch((err) => {
+                    console.log(err);
+                });
+        },
+        getItems() {
+            axios.get(apiurl + '/api/items')
+                .then((result) => {
+                    this.listItems = result.data.data;
+                }).catch((err) => {
+                    console.log(err);
+                });
+        },
+        getCustomers() {
+            axios.get(apiurl + '/api/customers')
+                .then((result) => {
+                    this.listCustomers = result.data.data;
                 }).catch((err) => {
                     console.log(err);
                 });
@@ -136,6 +160,8 @@ components: {
                 method = "PUT";
             }
 
+            if(this.form.customer_id == '') return alert('pilih customerid');
+            if(this.form.items[0].item_id == '') return alert('pilih item');
             axios({
                     method: method,
                     url: url,
@@ -146,7 +172,7 @@ components: {
                     this.getData();
                 })
                 .catch((error) => {
-                    // error.response.status Check status code
+                    alert(JSON.stringify(error.response.data));
                 }).finally(() => {
                     //Perform action in always
                 });
@@ -162,17 +188,17 @@ components: {
                     console.log(err);
                 });
         },
-        itemSelected (item) {
-      this.item = item;
-    },
-    setLabel (item) {
-      return item.name;
-    },
-    inputChange (text) {
-      // your search method
-      this.items = items.filter(item => item.name.contains(text));
-      // now `items` will be showed in the suggestion list
-    },
+        itemSelected(item) {
+            this.item = item;
+        },
+        setLabel(item) {
+            return item.name;
+        },
+        inputChange(text) {
+            // your search method
+            this.items = items.filter(item => item.name.contains(text));
+            // now `items` will be showed in the suggestion list
+        },
     },
 
     data() {
@@ -182,6 +208,8 @@ components: {
                 customer_id: '',
                 items: []
             },
+            listCustomers: [],
+            listItems: [],
             tableData,
             tableColumns,
             title: "Sales",
